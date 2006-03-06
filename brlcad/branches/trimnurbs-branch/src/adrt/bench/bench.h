c6a6d7bc4c3ec1c909431c856a2564a6eab25257
nobody <nobody@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2006-03-06 08:22:06 +0000

    This commit was manufactured by cvs2svn to create branch 'trimnurbs-branch'.

---
+++ brlcad/branches/trimnurbs-branch/src/adrt/bench/bench.h
@@ -0,0 +1,8 @@
+#ifndef _BENCH_H
+#define _BENCH_H
+
+
+extern	void bench(char *proj, int cache, int image);
+
+
+#endif
---
+++ brlcad/branches/trimnurbs-branch/src/other/blt/src/bltConfig.h
@@ -0,0 +1,2 @@
+/* utilize BRL-CAD's primary configure script which already has tests for everything BLT needs */
+#include "common.h"

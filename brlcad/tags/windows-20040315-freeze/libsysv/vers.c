a5e83da3230519a920b01ed55ae7e1c5555ad350
brlcad <brlcad@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2011-08-23 05:57:57 +0000

    similarly no reason to keep around the old cvs branch tags that were useful for tracking branch start points, end points, and branch termination.  relegated the legacy into svn history.

--- brlcad/tags/windows-20040315-freeze/libsysv/vers.c
+++
@@ -1,4 +0,0 @@
-char libsysv_version[] = "\
-@(#) BRL-CAD Release 6.0   BRL-CAD System V Compatibility Library\n\
-    Mon Apr  1 12:58:54 CST 2002, Compilation 1\n\
-    root@Mustang:/disk1/brlcad6.0/libsysv\n";
--- brlcad/tags/windows-20040315-freeze/libtermio/vers.c
+++
@@ -1,4 +0,0 @@
-char libtermio_version[] = "\
-@(#) BRL-CAD Release 6.0   BRL-CAD Terminal Mode Compatibility Library\n\
-    Mon Apr  1 13:31:13 CST 2002, Compilation 1\n\
-    root@Mustang:/disk1/brlcad6.0/libtermio\n";

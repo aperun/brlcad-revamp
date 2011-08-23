22b807af41c51386157dddf4200e315e4ca0eecf
brlcad <brlcad@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2011-08-23 13:42:42 +0000

    more removal of cvs branching artifacts where it was necessary to tag before and after merging, pushed into the depths of svn history

--- brlcad/tags/postmerge-20040405-ansi/h/conf.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONF_H
-#define CONF_H seen
-
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#endif
-
-#endif /* CONF_H */
--- brlcad/tags/premerge-20040404-ansi/nirt/vers.c
+++
@@ -1,4 +0,0 @@
-char version[] = "\
-@(#) BRL-CAD Release 6.0   Natalie's Interactive Ray Tracer\n\
-    Mon Apr  1 13:58:48 CST 2002, Compilation 1\n\
-    root@Mustang:/disk1/brlcad6.0/nirt\n";

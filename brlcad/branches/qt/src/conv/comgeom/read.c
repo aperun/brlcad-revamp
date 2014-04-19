a47be266670f45352159dcb5159f4d1529183fa7
starseeker <starseeker@2f96ce8b-6d43-0410-b8df-bffccc660ffb>
2014-04-19 01:06:22 +0000

    Create a branch for Qt experimentation

---
+++ brlcad/branches/qt/src/conv/comgeom/read.c
@@ -0,0 +1,115 @@
+/*                          R E A D . C
+ * BRL-CAD
+ *
+ * Copyright (c) 1989-2014 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ *
+ */
+/** @file comgeom/read.c
+ *
+ * This module contains all of the routines necessary to read in
+ * a COMGEOM input file, and put the information into internal form.
+ *
+ */
+
+#include "common.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#include "bu.h"
+
+extern FILE *infp;
+
+extern char name_it[16];		/* argv[3] */
+
+void namecvt(int n, char **cp, int c);
+
+int
+get_line(char *cp, int buflen, char *title)
+{
+    int c;
+    int count = buflen;
+
+    while ((c = fgetc(infp)) == '\n') /* Skip blank lines.		*/
+	;
+    while (c != EOF && c != '\n') {
+	*cp++ = c;
+	count--;
+	if (count <= 0) {
+	    printf("get_line(x%lx, %d) input record overflows buffer for %s\n",
+		   (unsigned long)cp, buflen, title);
+	    break;
+	}
+	c = fgetc(infp);
+    }
+    if (c == EOF)
+	return EOF;
+    while (count-- > 0)
+	*cp++ = 0;
+    return c;
+}
+
+
+int
+getint(char *cp, int start, size_t len)
+{
+    struct bu_vls vls = BU_VLS_INIT_ZERO;
+    int result;
+
+    bu_vls_strncpy(&vls, cp+start, len);
+    result = atoi(vls.vls_str);
+    bu_vls_free(&vls);
+
+    return result;
+}
+
+
+double
+getdouble(char *cp, int start, size_t len)
+{
+    struct bu_vls vls = BU_VLS_INIT_ZERO;
+    double result;
+
+    bu_vls_strncpy(&vls, cp+start, len);
+    result = atof(vls.vls_str);
+    bu_vls_free(&vls);
+
+    return result;
+}
+
+
+void
+namecvt(int n, char **cp, int c)
+{
+    char str[16];
+
+    sprintf(str, "%c%d%.13s", (char)c, n, name_it);
+    *cp = bu_strdup(str);
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ brlcad/branches/qt/src/conv/step/BRLCADWrapper.cpp
@@ -0,0 +1,256 @@
+/*                 BRLCADWrapper.cpp
+ * BRL-CAD
+ *
+ * Copyright (c) 1994-2014 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file step/BRLCADWrapper.cpp
+ *
+ * C++ wrapper to BRL-CAD database functions.
+ *
+ */
+
+#include "common.h"
+#include <stdlib.h>
+
+/* interface header */
+#include "./BRLCADWrapper.h"
+
+/* system headers */
+#include <sstream>
+#include <iostream>
+
+
+int BRLCADWrapper::sol_reg_cnt = 0;
+
+
+BRLCADWrapper::BRLCADWrapper()
+    : outfp(NULL), dbip(NULL)
+{
+}
+
+
+BRLCADWrapper::~BRLCADWrapper()
+{
+    Close();
+}
+
+bool
+BRLCADWrapper::load(std::string &flnm)
+{
+
+    /* open brlcad instance */
+    if ((dbip = db_open(flnm.c_str(), DB_OPEN_READONLY)) == DBI_NULL) {
+	bu_log("Cannot open input file (%s)\n", flnm.c_str());
+	return false;
+    }
+    if (db_dirbuild(dbip)) {
+	bu_log("ERROR: db_dirbuild failed: (%s)\n", flnm.c_str());
+	return false;
+    }
+
+    return true;
+}
+
+
+bool
+BRLCADWrapper::OpenFile(std::string &flnm)
+{
+    //TODO: need to check to make sure we aren't overwriting
+
+    /* open brlcad instance */
+    if ((outfp = wdb_fopen(flnm.c_str())) == NULL) {
+	bu_log("Cannot open output file (%s)\n", flnm.c_str());
+	return false;
+    }
+
+    // hold on to output filename
+    filename = flnm.c_str();
+
+    mk_id(outfp, "Output from STEP converter step-g.");
+
+    return true;
+}
+
+
+bool
+BRLCADWrapper::WriteHeader()
+{
+    db5_update_attribute("_GLOBAL", "HEADERINFO", "test header attributes", outfp->dbip);
+    db5_update_attribute("_GLOBAL", "HEADERCLASS", "test header classification", outfp->dbip);
+    db5_update_attribute("_GLOBAL", "HEADERAPPROVED", "test header approval", outfp->dbip);
+    return true;
+}
+
+
+bool
+BRLCADWrapper::WriteSphere(double *center, double radius)
+{
+    point_t pnt;
+    center[X] = 0.0;
+    center[Y] = 0.0;
+    center[Z] = 0.0;
+    VMOVE(pnt, center);
+    mk_sph(outfp, "s1", pnt, radius);
+    return true;
+}
+
+std::string
+BRLCADWrapper::GetBRLCADName(std::string &name)
+{
+    std::ostringstream str;
+    std::string strcnt;
+    struct bu_vls obj_name = BU_VLS_INIT_ZERO;
+    int len = 0;
+    char *cp,*tp;
+    static int start = 1;
+
+    for (cp = (char *)name.c_str(), len = 0; *cp != '\0'; ++cp, ++len) {
+	if (*cp == '@') {
+	    if (*(cp + 1) == '@')
+		++cp;
+	    else
+		break;
+	}
+	bu_vls_putc(&obj_name, *cp);
+    }
+    bu_vls_putc(&obj_name, '\0');
+    tp = (char *)((*cp == '\0') ? "" : cp + 1);
+
+    do {
+	bu_vls_trunc(&obj_name, len);
+	bu_vls_printf(&obj_name, "%d", start++);
+	bu_vls_strcat(&obj_name, tp);
+    }
+    while (db_lookup(outfp->dbip, bu_vls_addr(&obj_name), LOOKUP_QUIET) != RT_DIR_NULL);
+
+    return bu_vls_addr(&obj_name);
+}
+
+static MAP_OF_BU_LIST_HEADS heads;
+bool
+BRLCADWrapper::AddMember(const std::string &combname,const std::string &member,mat_t mat)
+{
+    MAP_OF_BU_LIST_HEADS::iterator i = heads.find(combname);
+    if (i != heads.end()) {
+	struct bu_list *head = (*i).second;
+	if (mk_addmember(member.c_str(), head, mat, WMOP_UNION) == WMEMBER_NULL)
+	    return false;
+    } else {
+	struct bu_list *head = NULL;
+
+	BU_ALLOC(head, struct bu_list);
+
+	BU_LIST_INIT(head);
+	if (mk_addmember(member.c_str(), head, mat, WMOP_UNION) == WMEMBER_NULL)
+	    return false;
+	heads[combname] = head;
+    }
+
+    return true;
+}
+
+bool
+BRLCADWrapper::WriteCombs()
+{
+    MAP_OF_BU_LIST_HEADS::iterator i = heads.begin();
+    while (i != heads.end()) {
+	std::string combname = (*i).first;
+	struct bu_list *head = (*i++).second;
+	unsigned char rgb[] = {200, 180, 180};
+
+	(void)mk_comb(outfp, combname.c_str(), head, 0, "plastic", "", rgb, 0, 0, 0, 0, 0, 0, 0);
+
+	mk_freemembers(head);
+
+	BU_FREE(head, struct bu_list);
+
+    }
+    heads.clear();
+    return true;
+}
+
+
+void
+BRLCADWrapper::getRandomColor(unsigned char *rgb)
+{
+    /* golden ratio */
+    static fastf_t hsv[3] = { 0.0, 0.5, 0.95 };
+    static double golden_ratio_conjugate = 0.618033988749895;
+    static fastf_t h = drand48();
+
+    h = fmod(h+golden_ratio_conjugate,1.0);
+    *hsv = h * 360.0;
+    bu_hsv_to_rgb(hsv,rgb);
+}
+
+
+bool
+BRLCADWrapper::WriteBrep(std::string name, ON_Brep *brep, mat_t &mat)
+{
+    std::ostringstream str;
+    std::string strcnt;
+    std::string sol = name + ".s";
+    std::string reg = name;
+
+    mk_brep(outfp, sol.c_str(), brep);
+    unsigned char rgb[] = {200, 180, 180};
+
+    BRLCADWrapper::getRandomColor(rgb);
+
+    struct bu_list head;
+    BU_LIST_INIT(&head);
+    if (mk_addmember(sol.c_str(), &head, mat, WMOP_UNION) == WMEMBER_NULL)
+	return false;
+
+    if (mk_comb(outfp, reg.c_str(), &head, 1, "plastic", "", rgb, 0, 0, 0, 0, 0, 0, 0) > 0)
+	return true;
+
+    return false;
+}
+
+struct db_i *
+BRLCADWrapper::GetDBIP()
+{
+    return dbip;
+}
+
+
+bool
+BRLCADWrapper::Close()
+{
+
+    if (outfp) {
+	wdb_close(outfp);
+	outfp = NULL;
+    }
+    if (dbip) {
+	db_close(dbip);
+	dbip = NULL;
+    }
+
+    return true;
+}
+
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
